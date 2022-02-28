45fd1b2cf0fe5185a681e54ab344c6876f2a9d9d
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-02-28 20:06:16 +0100

    prev et next boucle list vide

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ const App = () => {
 
     const {pokemonsList, isLoading, goPrev, goNext} = UsePokedex()
 
+
     if (isLoading) return <h1>Loading...</h1>
 
     return (
         <div className="App">
+            <ul>
+                {
+
+                    pokemonsList.results.map(pokemon => {
+                        <li key={pokemon.name}>{pokemon.name}</li>
+                    })
+                }
+            </ul>
             <button onClick={goPrev}>Prev</button>
             <button onClick={goNext}>Next</button>
         </div>
