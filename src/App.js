0578278e9da3364186b70092aa118928d75e316f
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-03-02 10:02:58 +0100

    router switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import UsePokedex from "./UsePokedex"
+import UsePokedex from './UsePokedex'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 const App = () => {
 
@@ -9,15 +10,24 @@ const App = () => {
     if (isLoading) return <h1>Loading...</h1>
 
     return (
-        <div className="App">
-            <ul>
-                {
-                    pokemonsList.results.map(pokemon => <li key={pokemon.name}>{pokemon.name}</li>)
-                }
-            </ul>
-            <button disabled={!pokemonsList.previous} onClick={goPrev}>Prev</button>
-            <button disabled={!pokemonsList.next} onClick={goNext}>Next</button>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <div className="App">
+                        <ul>
+                            {
+                                pokemonsList.results.map(pokemon => <li key={pokemon.name}>
+                                    <Link to={`/${pokemon.name}`}>{pokemon.name}</Link>
+                                </li>)
+                            }
+                        </ul>
+                        <button disabled={!pokemonsList.previous} onClick={goPrev}>Prev</button>
+                        <button disabled={!pokemonsList.next} onClick={goNext}>Next</button>
+                    </div>
+                </Route>
+                <Route path='/*'><h1>pokemon *</h1></Route>
+            </Switch>
+        </Router>
     );
 }
 
