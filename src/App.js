c466e063517d9d41dce1997820d809618d1b72ea
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-03-02 10:21:03 +0100

    creation context

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import UsePokedex from './hooks/UsePokedex'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import UsePokedex from './hooks/UsePokedex'
+import Home from "./pages/Home";
+import PokeContext from "./contexts/PokeContext";
 
 const App = () => {
 
     const {pokemonsList, isLoading, goPrev, goNext} = UsePokedex()
 
-
     if (isLoading) return <h1>Loading...</h1>
 
     return (
-        <Router>
-            <Switch>
-                <Route exact path='/'>
-                    <div className="App">
-                        <ul>
-                            {
-                                pokemonsList.results.map(pokemon => <li key={pokemon.name}>
-                                    <Link to={`/${pokemon.name}`}>{pokemon.name}</Link>
-                                </li>)
-                            }
-                        </ul>
-                        <button disabled={!pokemonsList.previous} onClick={goPrev}>Prev</button>
-                        <button disabled={!pokemonsList.next} onClick={goNext}>Next</button>
-                    </div>
-                </Route>
-                <Route path='/*'><h1>pokemon *</h1></Route>
-            </Switch>
-        </Router>
+        <PokeContext.Provider value={{pokemonsList, isLoading, goPrev, goNext}}>
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <Home></Home>
+                    </Route>
+                    <Route path='/*'><h1>pokemon *</h1></Route>
+                </Switch>
+            </Router>
+        </PokeContext.Provider>
     );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import {Link} from "react-router-dom";
+import PokeContext from "../contexts/PokeContext";
+
+const Home = () => {
+    const {pokemonsList, isLoading, goPrev, goNext} = useContext(PokeContext)
+    return (
+        <div>
+            <ul>
+                {
+                    pokemonsList.results.map(pokemon => <li key={pokemon.name}>
+                        <Link to={`/${pokemon.name}`}>{pokemon.name}</Link>
+                    </li>)
+                }
+            </ul>
+            <button disabled={!pokemonsList.previous} onClick={goPrev}>Prev</button>
+            <button disabled={!pokemonsList.next} onClick={goNext}>Next</button>
+        </div>
+    );
+};
+
+export default Home;
